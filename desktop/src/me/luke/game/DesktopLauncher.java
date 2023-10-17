1abb79324cba2f95f7443018002252018907fad4
Luke <64534456+lulkebit@users.noreply.github.com>
2023-10-17 11:08:47 +0200

    Initial commit

---
+++ desktop/src/me/luke/game/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package me.luke.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import me.luke.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Dungeon Game");
+		new Lwjgl3Application(new MyGdxGame(), config);
+	}
+}
