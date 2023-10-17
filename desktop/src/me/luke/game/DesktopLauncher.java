129a54b3d97cd153f9ab4ca323a3bda47787796a
Luke <64534456+lulkebit@users.noreply.github.com>
2023-10-17 11:44:39 +0200

    First simple game following libGDX wiki

--- desktop/src/me/luke/game/DesktopLauncher.java
+++ desktop/src/me/luke/game/DesktopLauncher.java
@@ -7,8 +7,11 @@ import me.luke.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
 		config.setForegroundFPS(60);
 		config.setTitle("Dungeon Game");
+		config.setWindowIcon("drop.png");
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
 }
